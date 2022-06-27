dc8f3a498690ae636c9bc01fbb1eaa87477bf48b
jaakkosy <jaakkosyrjamaki92@hotmail.com>
2022-06-27 13:32:31 +0300

    second commit

--- ClassLibraryJA/Class1.cs
+++ ClassLibraryJA/Class1.cs
@@ -4,5 +4,27 @@ namespace ClassLibraryJA
 {
     public class Class1
     {
+        static bool ReadBoolMethod(DateTime CompletionDate)
+        {
+
+            return (CompletionDate.Date > DateTime.UtcNow.Date);
+
+        }
+
     }
 }
+
+
+
+// 8hour / pv
+// 
+
+
+//5 pv, 8 h / pv = 40h / viikko. 
+
+//4. pv kohdalla Timespent = 5h.
+
+//    estimated - timespent = 
+
+    
+//
